ba91dfc6ebd286e05fd84ed3f332d2fcc56cce7d
fluxfw <fluxfw@fluxlabs.ch>
2022-10-20 15:39:06 +0200

    Phone and mobile on personal page

--- src/Adapter/PersonalData/PersonalData.mjs
+++ src/Adapter/PersonalData/PersonalData.mjs
@@ -1,3 +1,4 @@
+/** @typedef {import("../AreaCode/AreaCode.mjs").AreaCode} AreaCode */
 /** @typedef {import("../Country/Country.mjs").Country} Country */
 /** @typedef {import("./FilledPersonalData.mjs").FilledPersonalData} FilledPersonalData */
 /** @typedef {import("../Language/Language.mjs").Language} Language */
@@ -5,5 +6,5 @@
 /** @typedef {import("../Salutation/Salutation.mjs").Salutation} Salutation */
 
 /**
- * @typedef {{salutations: Salutation[], "registration-number-format": string, "registration-number-example": string, countries: Country[], "extra-address-line-example": string, "min-house-number": number, "max-house-number": number, "min-postal-code": number, "max-postal-code": number, places: Place[], languages: Language[], "min-birth-date": string, "max-birth-date": string, "old-age-survivar-insurance-number-format": string, "old-age-survivar-insurance-number-example": string, values: FilledPersonalData | null}} PersonalData
+ * @typedef {{salutations: Salutation[], "registration-number-format": string, "registration-number-example": string, countries: Country[], "extra-address-line-example": string, "min-house-number": number, "max-house-number": number, "min-postal-code": number, "max-postal-code": number, places: Place[], "area-codes": AreaCode[], "phone-format": string, "phone-example": string, languages: Language[], "min-birth-date": string, "max-birth-date": string, "old-age-survivar-insurance-number-format": string, "old-age-survivar-insurance-number-example": string, values: FilledPersonalData | null}} PersonalData
  */
