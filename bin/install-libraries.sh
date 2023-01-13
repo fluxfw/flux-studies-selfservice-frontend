d4286326d1d9a743e2c69aef2dd8542a71494d60
fluxfw <fluxfw@fluxlabs.ch>
2023-01-13 14:30:14 +0100

    libraries

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -24,7 +24,7 @@ installLibrary flux-css-api https://github.com/fluxfw/flux-css-api/archive/refs/
 
 installLibrary flux-hash-api https://github.com/fluxfw/flux-hash-api/archive/refs/tags/v2023-01-06-2.tar.gz
 
-installLibrary flux-http-api https://github.com/fluxfw/flux-http-api/archive/refs/tags/v2023-01-12-2.tar.gz
+installLibrary flux-http-api https://github.com/fluxfw/flux-http-api/archive/refs/tags/v2023-01-13-1.tar.gz
 
 installLibrary flux-json-api https://github.com/fluxfw/flux-json-api/archive/refs/tags/v2023-01-06-1.tar.gz
 
