26a4254daf429f62794959c0a31d605bd7f87b45
fluxfw <fluxfw@fluxlabs.ch>
2023-03-27 15:45:34 +0200

    `flux-pwa-api`

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -31,7 +31,7 @@ installLibrary flux-loading-spinner https://github.com/fluxfw/flux-loading-spinn
 
 installLibrary flux-localization-api https://github.com/fluxfw/flux-localization-api/archive/refs/tags/v2023-03-24-1.tar.gz
 
-installLibrary flux-pwa-api https://github.com/fluxfw/flux-pwa-api/archive/refs/tags/v2023-03-27-1.tar.gz
+installLibrary flux-pwa-api https://github.com/fluxfw/flux-pwa-api/archive/refs/tags/v2023-03-27-2.tar.gz
 
 installLibrary flux-pwa-generator https://github.com/fluxfw/flux-pwa-generator/archive/refs/tags/v2023-03-22-1.tar.gz
 
