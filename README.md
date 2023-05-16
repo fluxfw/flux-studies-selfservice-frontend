bca91433920dfa6a622e16a3f74bd6d6bc766542
fluxfw <fluxfw@fluxlabs.ch>
2023-05-16 13:17:58 +0200

    `flux-overlay`

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # flux-studis-selfservice-frontend
 
 ```Dockerfile
-RUN (mkdir -p /path/to/web-root && cd /path/to/web-root && wget -O - https://github.com/fluxfw/flux-studis-selfservice-frontend/releases/download/vYYYY-MM-DD-I/flux-studis-selfservice-frontend-vYYYY-MM-DD-I-build.tar.gz | tar -xz --strip-components=1)
+RUN (mkdir -p /path/to/web-root && cd /path/to/web-root && wget -O - https://github.com/fluxfw/flux-studis-selfservice-frontend/releases/download/%version%/flux-studis-selfservice-frontend-%version%-build.tar.gz | tar -xz --strip-components=1)
 ```
--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -19,7 +19,7 @@ installLibrary() {
 
 checkAlreadyInstalled
 
-installLibrary flux-button-group https://github.com/fluxfw/flux-button-group/archive/refs/tags/v2023-05-04-1.tar.gz
+installLibrary flux-button-group https://github.com/fluxfw/flux-button-group/archive/refs/tags/v2023-05-16-1.tar.gz
 
 installLibrary flux-color-scheme https://github.com/fluxfw/flux-color-scheme/archive/refs/tags/v2023-04-24-1.tar.gz
 
@@ -31,7 +31,7 @@ installLibrary flux-loading-spinner https://github.com/fluxfw/flux-loading-spinn
 
 installLibrary flux-localization-api https://github.com/fluxfw/flux-localization-api/archive/refs/tags/v2023-05-04-1.tar.gz
 
-installLibrary flux-overlay https://github.com/fluxfw/flux-overlay/archive/refs/tags/v2023-05-10-1.tar.gz
+installLibrary flux-overlay https://github.com/fluxfw/flux-overlay/archive/refs/tags/v2023-05-16-1.tar.gz
 
 installLibrary flux-pwa-api https://github.com/fluxfw/flux-pwa-api/archive/refs/tags/v2023-05-01-1.tar.gz
 
